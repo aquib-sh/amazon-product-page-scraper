69e88d294cb7ced4fad465b194c812707760382f
aquib-sh <shaikhaquib394@gmail.com>
2022-05-13 17:55:14 +0530

    Add base requirements

---
+++ config.py
@@ -0,0 +1,10 @@
+fb_email = "prufrock66@protonmail.com"
+fb_pswd = "sxRG10@("
+fb_group_link = "https://m.facebook.com/groups/923023231097937"
+fb_group_announc = "https://m.facebook.com/groups/923023231097937?view=announcements"
+
+yt_email = "tuneloonsmonkey@gmail.com"
+yt_pswd = "r3n3gad3"
+
+soundiiz_login = "soopageek"
+soundiiz_pswd  = "SZoi45^&"
