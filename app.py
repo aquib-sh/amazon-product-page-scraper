363609ef5332932036030ca8bdadb508ff63c200
aquib-sh <shaikhaquib394@gmail.com>
2022-05-15 14:21:01 +0530

    Add requirements, integrate logger to app

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 import config
 from io_handler import CSVReader, CSVWriter, XLSXReader, XLSXWriter
+from log import Logger
 from scraper import ProductScraper
 
 
 class App:
     def __init__(self):
+        self.logger = Logger()
         self.scraper = ProductScraper()
         self.reader = self.__create_reader(config.INPUT_FILE)
-        self.writer = self.__create_writer()
+        self.writer = self.__create_writer(config.OUTPUT_FILE)
 
     def __create_reader(self, input_file):
+        self.logger.info(f"Reading {input_file}")
         if input_file.endswith("csv"):
             return CSVReader(input_file)
         elif input_file.endswith("xlsx"):
@@ -28,11 +31,26 @@ class App:
     def run(self):
         i = 0
         for isbn in self.reader.readISBN():
+            self.logger.info(f"Scraping for ISBN: {isbn}")
             info = self.scraper.scrape(isbn)
+
+            if info is None:
+                self.logger.error(f"No URL found for {isbn}")
+                continue
 
             if not i: 
                 self.writer.set_columns(list(info.keys()))
-                i = not i
 
+            i += 1
+
+            self.logger.info("Adding to buffer")
             self.writer.add_to_buffer(info)
+            if i % 20 == 0:
+                self.writer.write()
+                self.logger.info(f"Saved {i} results to {config.OUTPUT_FILE}")
+
         self.writer.write()
+        self.logger.info(f"Saved the results to {config.OUTPUT_FILE}")
+
+if __name__ == "__main__":
+    App().run()
--- scraper.py
+++ scraper.py
@@ -11,6 +11,14 @@ class ProductScraper:
         self.URL = "https://www.amazon.in/dp/{isbn}"
         self.soup = None
 
+    def __url_not_found(self) -> bool:
+        not_found_text = "We're sorry. The Web address you entered is not a functioning page on our site."
+        tdata = self.soup.find_all("td")
+        for data in tdata:
+            if ((data is not None) and (not_found_text in data.text)): 
+                return True
+        return False
+
     def __get_product_title(self):
         title = self.soup.find("span", {"id":"productTitle"}).text.strip()
         return title
@@ -28,44 +36,66 @@ class ProductScraper:
     def __get_ratings(self):
         # customer ratings is present in the format of 3 out of 5 starts \n total_reviews,
         # for now we don't want the number of total_reviews, so we split it.        
-        customer_rating = self.soup.find("div", {"id":"averageCustomerReviews"})        
-        num_of_ratings = ""
-        if customer_rating == None:
-            customer_rating = ""
-        else:
-            customer_rating, num_of_ratings = customer_rating.text.strip().split("\n")
-            customer_rating = customer_rating.strip()
-            num_of_ratings = num_of_ratings.strip()
-        return customer_rating, num_of_ratings
+        try:
+            customer_rating = self.soup.find("div", {"id":"averageCustomerReviews"})        
+            num_of_ratings = ""
+            if customer_rating == None:
+                customer_rating = ""
+            else:
+                customer_rating, num_of_ratings = customer_rating.text.strip().split("\n")
+                customer_rating = customer_rating.strip()
+                num_of_ratings = num_of_ratings.strip()
+            return customer_rating, num_of_ratings
+        except Exception as e:
+            print(e)
+            return "", ""
 
     def __get_image_url(self):
         # Image list for product is present as a key value pair in data-a-dynamic-image attribute
         # We will get that attribute data and evaluate it to make it a python dict
         # then we create a list from the keys of that dict, the dict contains image url and their resolution
         # we just need the image url which is present in key. We will only select 1st image.
-        img_li = self.soup.find("li", {"class":"image"})
-        if img_li == None:
-            img_li = self.soup.find("div", {"id":"img-canvas"})
-        disp_img_attr_str = img_li.find("img")['data-a-dynamic-image']
-        disp_img_attr = eval(disp_img_attr_str)
-        disp_img_list = list(disp_img_attr.keys())
-        product_image_url = disp_img_list[0]
-        return product_image_url
+        try:
+            img_li = self.soup.find("li", {"class":"image"})
+            if img_li == None:
+                img_li = self.soup.find("div", {"id":"img-canvas"})
+            disp_img_attr_str = img_li.find("img")['data-a-dynamic-image']
+            disp_img_attr = eval(disp_img_attr_str)
+            disp_img_list = list(disp_img_attr.keys())
+            product_image_url = disp_img_list[0]
+            return product_image_url
+        except Exception as e:
+            print(e)
+            return ""
+
+    def __get_category(self) -> str:
+        try:
+            categories = self.soup.find("ul", {"class":"a-unordered-list"}).find_all("li")
+            category = " ".join([c.text.strip() for c in categories if c is not None])
+            return category
+        except Exception as e:
+            print(e)
+            return ""
 
     def scrape(self, isbn):
-        self.product_url = self.URL.format(isbn=isbn)
-        self.__bot.move(self.product_url)
+        product_url = self.URL.format(isbn=isbn)
+        self.__bot.move(product_url)
         self.soup = self.__prepare_soup()
+        if self.__url_not_found():
+            return None
 
         title = self.__get_product_title()
+        category = self.__get_category()
         price = self.__get_product_price()
         rating, n_of_rating = self.__get_ratings()
         image_url = self.__get_image_url()
 
         return {
-            "title":title,
-            "price":price,
-            "customer_ratings":rating,
+            "product_url": product_url,
+            "title": title,
+            "category": category,
+            "price": price,
+            "customer_ratings": rating,
             "number_of_ratings": n_of_rating,
             "image_url": image_url
         }
